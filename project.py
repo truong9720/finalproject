6075e2499975a627a98fe8619b2ecc1469894e70
truong9720 <ngotruongt71@example.com>
2025-02-06 17:43:37 +0700

    new

--- project.py
+++ project.py
@@ -1,5 +1,7 @@
 import requests
 from datetime import datetime
+import json
+import sys
 
 
 def getposts(topic: str, days, sortby):
@@ -16,10 +18,54 @@ def getposts(topic: str, days, sortby):
     return result
 
 
+def get_top_headlines(keyword):
+    KEY = "4918a39177f64b02a96be747c45b4296"
+    url = f"https://newsapi.org/v2/top-headlines?country={
+        keyword}&apiKey={KEY}"
+    feedback = requests.get(url)
+    result = feedback.json()
+    if len(result["articles"]) == 0:
+        print("There is no headlines on this keyword")
+        return result
+
+
+def save_news(result, filename='news.json'):
+    try:
+        articles = []
+        for post in result["articles"]:
+            articles.append(
+                {
+                    "title": post["title"],
+                    "description": post["description"],
+                    "url": post["url"],
+                    "author": post["author"],
+                    "urlToImage": post["urlToImage"]
+                }
+            )
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(articles, f, indent=4)
+
+        print(f"\nSuccessfully saved news to {filename}")
+
+    except Exception as e:
+        print(f"Error while saving news to file: {e}")
+
+
+def menu():
+    print("\n===News Application ===")
+    print("1.Search news")
+    print("2.Get top headlines")
+    print("3.Save news to file")
+    print("4.Exit")
+    return input("Choose one option betwwen 1-4:")
+
+
 def printposts(result):
     count = 0
     for post in result["articles"]:
         if post["title"] != "[Removed]":
+            count = count + 1
             count = count + 1
             print("Title: {} ".format(post["title"]))
             print("Description: {} ".format(post["description"].strip()))
@@ -32,11 +78,37 @@ def printposts(result):
 
 
 def main():
-    topic = input("Enter Topic: ")
-    days = input("Days (e.g YYYY-MM-DD): ")
-    sortby = input("Sort By (e.g Newer ; Older ; Popularity) : ")
-    result = getposts(topic, days, sortby)
-    printposts(result)
+    news = None
+    while True:
+        choice = menu()
+        if choice == "1":
+            topic = input("Enter Topic: ")
+            days = input("Days (e.g YYYY-MM-DD): ")
+            sortby = input("Sort By (e.g Newer ; Older ; Popularity) : ")
+            try:
+                news = getposts(topic, days, sortby)
+                printposts(news)
+            except Exception as e:
+                print(f"Error: {e}")
+        elif choice == "2":
+            try:
+                keyword = input("Enter a keyword: ")
+                news = get_top_headlines(keyword)
+                printposts(news)
+            except Exception as e:
+                print(f"Error: {e}")
+        elif choice == "3":
+            if news:
+                filename = input(
+                    "Enter a file name: (if not, the default name is news.json)")
+                save_news(news, filename)
+            else:
+                print("There are not any news before!")
+        elif choice == "4":
+            print("Exiting .....")
+            sys.exit(0)
+        else:
+            print("Invalid inputing!!!")
 
 
 if __name__ == "__main__":
---
+++ test_project.py
@@ -0,0 +1,52 @@
+import json
+import sys
+import pytest
+import os
+from project import getposts, get_top_headlines, printposts, save_news
+
+
+def get_test_data():
+    return getposts("technology", "2025-01-06", "popularity")
+
+
+def test_getposts():
+    result = get_test_data()
+    assert isinstance(result, dict)
+    assert len(result["articles"]) > 0
+
+
+def test_get_top_headlines():
+    result = get_top_headlines("trump")
+    assert isinstance(result, dict)
+    assert "articles" in result
+
+
+def test_printposts(capsys):
+    test_data = get_test_data()
+    printposts(test_data)
+    captured = capsys.readouterr()
+
+    assert "Title" in captured.out
+    assert "Description" in captured.out
+    assert "Url" in captured.out
+
+
+def test_save_news():
+    filename = "test_news.json"
+    test_data = get_test_data()
+    save_news(test_data, filename)
+
+    assert os.path.exists(filename)
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        saved_data = json.load(f)
+    assert len(saved_data) > 0
+
+    assert "title" in saved_data[0]
+    assert "description" in saved_data[0]
+
+    assert "title" in saved_data[0]
+    assert "description" in saved_data[0]
+    assert "url" in saved_data[0]
+    assert "author" in saved_data[0]
+    assert "urlToImage" in saved_data[0]
